04f2ef285e4d6a90840f90d1fdb63aeecb7029c7
takuan517 <ishitaku-2000517@keio.jp>
2021-01-02 23:23:16 +0900

    add template

--- main.go
+++ main.go
@@ -25,7 +25,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	//チャットルームを開始します
+	go r.run()
 	//Webサーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
